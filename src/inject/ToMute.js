323907f1fe1c22242a9c319c1e99bc1980155553
amastov <amastov1@gmail.com>
2017-05-04 03:35:44 -0700

    use right component

--- src/inject/ToMute.js
+++ src/inject/ToMute.js
@@ -6,11 +6,11 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 		var observer = new MutationObserver(function(mutations, observer) {
-			if (document.getElementsByClassName("player-ad-overlay js-player-ad-overlay")[0].getAttribute("target") != "_blank" 
+			if (document.getElementsByClassName("player-ad-overlay js-player-ad-overlay")[0].getAttribute("href") != "" 
 				&& document.getElementsByClassName("player-tip")[2].getAttribute("data-tip") == "Mute") {
 				document.getElementsByClassName("player-button player-button--volume qa-control-volume")[0].click();
 			}
-			if (document.getElementsByClassName("player-ad-overlay js-player-ad-overlay")[0].getAttribute("target") == "_blank" 
+			if (document.getElementsByClassName("player-ad-overlay js-player-ad-overlay")[0].getAttribute("href") == "" 
 				&& document.getElementsByClassName("player-tip")[2].getAttribute("data-tip") == "Unmute") {
 				document.getElementsByClassName("player-button player-button--volume qa-control-volume")[0].click();
 			}
