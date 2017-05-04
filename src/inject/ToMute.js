3858030bc61845140c8fc5b8d844e7f40bf63814
amastov <amastov1@gmail.com>
2017-05-04 00:33:16 -0700

    Make it work

---
+++ src/inject/ToMute.js
@@ -0,0 +1,27 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+		clearInterval(readyStateCheckInterval);
+		console.log("on twitch");
+		// ----------------------------------------------------------
+		// This part of the script triggers when page is done loading
+		MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+		var observer = new MutationObserver(function(mutations, observer) {
+			console.log("mutate");
+			if (document.getElementsByClassName("player-ad-overlay js-player-ad-overlay")[0].getAttribute("target") != "_blank" 
+				&& document.getElementsByClassName("player-tip")[2].getAttribute("data-tip") == "Mute") {
+				document.getElementsByClassName("player-button player-button--volume qa-control-volume")[0].click();
+			}
+			if (document.getElementsByClassName("player-ad-overlay js-player-ad-overlay")[0].getAttribute("target") == "_blank" 
+				&& document.getElementsByClassName("player-tip")[2].getAttribute("data-tip") == "Unmute") {
+				document.getElementsByClassName("player-button player-button--volume qa-control-volume")[0].click();
+			}
+		});
+		observer.observe(document.getElementsByClassName("player-ad-overlay js-player-ad-overlay")[0], {
+			attributes: true
+		});
+		// ----------------------------------------------------------
+
+	}
+	}, 10);
+});
