0a645ee062a37333db5c53af741cbf01f13bacdd
Adam Mastov <amastov1@gmail.com>
2022-01-10 20:11:59 -0700

    Rewrite this thing now that I'm smarter. I'm still not that smart.

--- src/inject/ToMute.js
+++ src/inject/ToMute.js
@@ -1,24 +1,45 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-		var observer = new MutationObserver(function(mutations, observer) {
-			if (document.getElementsByClassName("player-ad-overlay js-player-ad-overlay")[0].getAttribute("href") != "" 
-				&& document.getElementsByClassName("player-tip")[2].getAttribute("data-tip") == "Mute") {
-				document.getElementsByClassName("player-button player-button--volume qa-control-volume")[0].click();
-			}
-			if (!document.getElementsByClassName("player-ad-overlay js-player-ad-overlay")[0].getAttribute("href") 
-				&& document.getElementsByClassName("player-tip")[2].getAttribute("data-tip") == "Unmute") {
-				document.getElementsByClassName("player-button player-button--volume qa-control-volume")[0].click();
-			}
-		});
-		observer.observe(document.getElementsByClassName("player-ad-overlay js-player-ad-overlay")[0], {
-			attributes: true
-		});
-		// ----------------------------------------------------------
-	}
-	}, 10);
+chrome.extension.sendMessage({}, function (response) {
+  var readyStateCheckInterval = setInterval(function () {
+    if (document.readyState === 'complete') {
+      clearInterval(readyStateCheckInterval);
+      // ----------------------------------------------------------
+      // This part of the script triggers when page is done loading
+      MutationObserver =
+        window.MutationObserver || window.WebKitMutationObserver;
+      var observer = new MutationObserver(function (mutations, observer) {
+        let spans = document.getElementsByTagName('span');
+        let spanArr = spans && spans.length ? [...spans] : undefined;
+        let isAdPlaying = spanArr
+          ? spanArr.some(
+              (x) =>
+                x.hasAttribute('data-a-target') &&
+                x.getAttribute('data-a-target') == 'video-ad-label'
+            )
+          : undefined;
+
+        let buttons = document.getElementsByTagName('button');
+        let buttonArr = buttons && buttons.length ? [...buttons] : undefined;
+        let muteButton =
+          buttonArr && buttonArr.length
+            ? buttonArr.find(
+                (x) =>
+                  x.hasAttribute('data-a-target') &&
+                  x.getAttribute('data-a-target') == 'player-mute-unmute-button'
+              )
+            : undefined;
+        let isMuted = muteButton
+          ? muteButton.getAttribute('aria-label') == 'Unmute (m)'
+          : undefined;
+
+        if (isAdPlaying != isMuted) {
+          muteButton.click();
+        }
+      });
+      observer.observe(document.getElementsByClassName('video-player')[0], {
+        attributes: true,
+        subtree: true,
+      });
+      // ----------------------------------------------------------
+    }
+  }, 10);
 });
